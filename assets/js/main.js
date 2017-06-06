8831e046041cf16abce09ed2b55564a2571eef85
Fran Ruiz-Tagle <francisca.ruiztagle@gmail.com>
2017-06-05 20:19:11 -0400

    navbar funcionando, boton 2 aparece, falta editar estilos boton 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,17 @@ function cambioColor() {
     var distancia = window.pageYOffset || document.documentElement.scrollTop;
     var nuevonav = document.getElementById("nav");
     var logo = document.getElementById("logo"); //el logo blanco
+    var boton = document.getElementById("btnTwo");
 
     if (distancia > 100) {
         nuevonav.classList.add("nuevoNav");
         logo.setAttribute("src", "assets/images/logo-pink.png");
+        boton.classList.remove("hidden")
+        document.getElementById("btnTwo").setAttribute("visible", true); //
     } else {
         nuevonav.classList.remove("nuevoNav"); //así remuevo este nav nuevo y vuelvo al inicial cuando estoy a menos de 100px de distancia
         logo.setAttribute("src", "assets/images/logo-white.png");
+        boton.classList.add("hidden");
     }
 }
 window.addEventListener("scroll", cambioColor);
